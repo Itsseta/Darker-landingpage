1ca33fdeeeb6d9532a1ff4640273b2ed12117946
Ittseta <122336611+Itsseta@users.noreply.github.com>
2023-07-10 23:13:33 +0900

    Initial commit

---
+++ src/screens/Frame/Frame.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="nav">
+              <div className="div">
+                <div className="link">
+                  <div className="text-wrapper">Login</div>
+                </div>
+                <div className="button-request">
+                  <div className="text-wrapper-2">Try Darker</div>
+                </div>
+              </div>
+              <div className="text-wrapper-3">DARKER</div>
+            </div>
+            <div className="rectangle" />
+            <p className="heading-immerse">With the consent of the creators</p>
+            <div className="heading-immerse-2">With sustainable revenue model</div>
+            <div className="heading-immerse-3">With Creator centric models</div>
+            <p className="p">With fair compensation for creators</p>
+            <div className="rectangle-2" />
+            <div className="rectangle-3" />
+            <h1 className="h-1">AI with human base.</h1>
+            <p className="text-wrapper-4">Deep learning models built with creaters</p>
+            <div className="heading-immerse-4">We Want Solve</div>
+          </div>
+          <div className="overlap-2">
+            <img
+              className="img"
+              alt="Rectangle"
+              src="https://generation-sessions.s3.amazonaws.com/aff219ce30a5cfe505e68002ce3ae679/img/rectangle-342.png"
+            />
+            <div className="rectangle-4" />
+            <div className="rectangle-5" />
+            <div className="rectangle-6" />
+          </div>
+          <div className="overlap-group-2">
+            <div className="div-framer">
+              <div className="div-framer-2">
+                <div className="div-framer-cbktgn">
+                  <div className="text-wrapper-5">Anti Copyright</div>
+                </div>
+                <div className="div-sc" />
+                <div className="div-framer-cbktgn">
+                  <div className="text-wrapper-6">Exclusive</div>
+                </div>
+              </div>
+              <div className="div-sc-efaf" />
+              <div className="div-framer-2">
+                <div className="div-framer-cbktgn">
+                  <div className="text-wrapper-7">Limited</div>
+                </div>
+                <div className="div-sc" />
+                <div className="div-framer-cbktgn">
+                  <div className="text-wrapper-8">None sustainability</div>
+                </div>
+              </div>
+            </div>
+            <p className="heading-immerse-5">Problems of traditional machine learning training methods</p>
+            <div className="rectangle-7" />
+            <div className="rectangle-8" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
